b1ca473d4f02b0877dc38e977bf39cc1afb21797
PengClever <2062166376@qq.com>
2023-05-12 23:21:32 +0800

    update DebugLog

--- src/main/java/org/example/Communication.java
+++ src/main/java/org/example/Communication.java
@@ -17,7 +17,6 @@ public class Communication {
     Vector<String> lastQueries;
     Vector<String> lastQueriesReply;
     DebugLog log;
-    long resetCount;
     public Communication(LearnerConfig config) throws Exception {
         // Test CS communication
 //        System.out.println("symbol: " + "ADU1CWRD88:97:46:2C:9A:CE");
@@ -42,7 +41,6 @@ public class Communication {
         lastQueries = new Vector<>(100);
         lastQueriesReply = new Vector<>(100);
         log = new DebugLog();
-        resetCount = 0;
         // Test CS communication
 //        decryptSymbol(receiveSymbol(encryptSymbol("ADU1CWR")));
 //        receiveSymbol("111");
@@ -304,6 +302,7 @@ public class Communication {
 
     public String processSymbol(String symbol) throws Exception {
         if(symbol != null){
+            // 本地简单配置测试
             if (symbol.equals("a"))
                 return "z";
             if (symbol.equals("b"))
@@ -337,7 +336,7 @@ public class Communication {
             if (i == queries.size()) {
                 System.out.println("Cache Reply: " + lastQueriesReply.get(i - 1));
                 queriesReply.add(lastQueriesReply.get(i - 1));
-                log.addLog(lastQueries.get(i - 1), lastQueriesReply.get(i - 1), 2);
+                log.addLog(lastQueries.get(i - 1), lastQueriesReply.get(i - 1), 1);
                 return lastQueriesReply.get(i - 1);
             }
             // 不使用缓存时处理字符串，将字母表中的表达式转换为CS通信中要求的格式
@@ -356,7 +355,7 @@ public class Communication {
                     lastQueriesReply.add(queriesReply.get(j));
                 }
             }
-            log.addLog(outSymol, symbol, 1);
+            log.addLog(outSymol, symbol, 0);
             return symbol;
         }
         else {
@@ -366,7 +365,7 @@ public class Communication {
 
     public void reset() throws Exception {
         decryptSymbol(receiveSymbol(encryptSymbol("RESET")), true);
-        log.addLog("Reset", resetCount++);
+        log.addResetCount();
         queries = new Vector<>(100);
         queriesReply = new Vector<>(100);
     }
--- src/main/java/org/example/DebugLog.java
+++ src/main/java/org/example/DebugLog.java
@@ -2,38 +2,39 @@ package org.example;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.util.Objects;
 
 public class DebugLog {
     static String filename = "result/output/DebugLog.txt";
     File file;
     FileOutputStream fos;
+    long resetCount;
+
     public DebugLog() throws Exception {
+        resetCount = -1;
         file = new File(filename);
-        if(file.exists()) {
-            try {
-                file.delete();
-            } catch (Exception e) {
-                System.out.println("创建调试日志文件失败");
-            }
-        }
-        file.createNewFile();
-        fos = new FileOutputStream(file, true);
+        if(file.exists())
+            if (!file.delete())
+                System.out.println("日志文件初始化失败：删除失败");
+        if (file.createNewFile())
+            fos = new FileOutputStream(file, true);
+        else
+            System.out.println("日志文件初始化失败：创建失败");
     }
 
-    public void addLog(String symbol, long count) throws Exception {
-        if (Objects.equals(symbol, "Reset")){
-            fos.write(("[" + count + "] " + "\n").getBytes());
-        }
+    public void addResetCount() {
+        resetCount++;
     }
 
     public void addLog(String outSymbol, String inSymbol, int mode) throws Exception {
         switch (mode) {
-            case 1:
-                fos.write(("[CS] " + outSymbol + " -> " + inSymbol + "\n").getBytes());
+            case 0:
+                fos.write(("[" + resetCount + "][CS] " + outSymbol + " -> " + inSymbol + "\n").getBytes());
                 break;
-            case 2:
-                fos.write(("[Cache] " + outSymbol + " -> " + inSymbol + "\n").getBytes());
+            case 1:
+                fos.write(("[" + resetCount + "][Cache] " + outSymbol + " -> " + inSymbol + "\n").getBytes());
+                break;
+            default:
+                System.out.println("日志添加失败");
         }
     }
 }
